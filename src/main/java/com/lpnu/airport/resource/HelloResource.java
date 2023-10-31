e91c96719633ea5d8dc4563d6d1718b7ae0be89b
unknown <dimitrius1308@gmail.com>
2023-10-31 20:19:56 +0200

    initial commit

---
+++ src/main/java/com/lpnu/airport/resource/HelloResource.java
@@ -0,0 +1,12 @@
+package com.lpnu.airport.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource  {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
